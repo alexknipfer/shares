3f71056324b2c5b57a9e66a7758772cb48af3c69
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-21 21:41:40 -0600

    google api for stocks

--- src/modules/StockView/StockView.js
+++ src/modules/StockView/StockView.js
@@ -4,9 +4,28 @@ import React, { Component } from 'react'
 import { observer } from 'mobx-react'
 
 const StockView = observer(class StockView extends Component {
+
+    newPrice = (arr) => {
+        currentPrice = arr[0]["l"]
+        document.getElementById("price").innerHTML = currentPrice
+    }
+
+    componentDidMount() {
+        const request = require('request')
+        request("https://finance.google.com/finance/info?client=ig&q=goog", (error, response, body) => {
+            body = body.slice(3)
+            body = JSON.parse(body)
+            console.log(body)
+            newPrice(body)
+        })
+    }
+
     render() {
         return (
-            <h3>StockView Component</h3>
+            <div>
+                <div className="the-price" id="price">$1000</div>
+                <h3>StockView Component</h3>
+            </div>
         )
     }
 })
